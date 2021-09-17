db2461c17d445c3f9cde5d9d92cc99a859178689
williansamer <williansamer@gmail.com>
2021-09-17 12:26:37 -0300

    add a extensão 'cors' e atribuindo o domínio localhost a ter acesso a apo

--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 Criando um Mural de Avisos (Front e Back)
 
-Criado um Mural de Avisos em linguagens HTML5, BootStrap, JavaScript e Node
+O intuito do projeto é aplicar os conhecimentos sobre API REST. 
+
+>>> tecnologias usadas
+
+- HTML
+- Bootstrap
+- Javascript
+- Node JS
+
+>>> Funcionalidades
+- Criar um novo post
+- Exclui o post
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors"); //importando o cors
 const posts = require("../model/posts") //importando o obj 'posts'
+
+const options = {origin: "http://localhost:3000"} //Sem determinar algo para o 'cors'(linha 8), Iria liberar para que TODOS os domínios disponíveis tivessem acesso ao core(api) do site. Não queremos isto. Por este motivo, criamos este 'options' com obj informando qual outro domínio terá acesso(localhost) além do padrão(192.168.1.103). Ou seja, Somente 2 domínios agora terão acesso ao core(api) do nosso sistema.
+
+router.use(cors(options)); // criando uma rota para o middleware 'cors'. Faz com que mais de 1 domínio, consiga acessar a api do 'site'. O parâmetro 'options' é explicado na linha 6..
+//Sem o middleware cors, por segurança, o CORS padrão bloqueia todos os demais domínios além do path(caminho/domínio padrão) designado a ele. Por ex neste caso, é o "http://192.168.1.103:3000"
 
 router.get("/all", (req, res)=>{ //LEITURA DE TODOS OS POSTS
     res.json(JSON.stringify(posts.getAll())); // Chamando a função 'posts.getAll()' que então retorna 'this.posts'. obs: Lembre-se de colocar os parenteses
