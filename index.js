a671ed1851c46b39c89e9da700932127806498b5
williansamer <williansamer@gmail.com>
2021-09-21 16:06:34 -0300

    conf variáveis de ambiente

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path")
-
-const PORT = 3000;
+const dotenv = require("dotenv");
 const route = require("./routes/api") // importando
+
+dotenv.config(); //Pacote responsável por configurar variáveis de ambiente. O 'config()' ele manda para o arquivo '.env' que por padrão fica(ou tem que ficar) no diretório raiz.
+
+const port = process.env.PORT;//Variáveis de ambiente(process.env.PORT). Dentro o arquivo '.env' foi configurado a 'PORT'
 
 app.use("/api", route); //Aqui estamos rodando um middleware(route) para aplicar o GET, POST e DELETE(que estão neste 'routes', dentro do path(caminho) '/api')
 app.use(express.static(path.join(__dirname, "public"))); //Criando um HTML estático e juntando o caminho do diretório("__dirname") com o diretório("public"). Ou seja, dentro deste diretório, irá procurar e abrir o arquivo com nome padrão que é 'index.html'
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log(`Server running..${PORT}`);
+app.listen(port || 3000, ()=>{ 
+    //console.log(process.env.API_KEY); //Caso necessite de uma 'API_KEY' - tipo de autenticação/proteção dos dados
+    console.log(`Server running..${port}`);
 })
 
 
--- readme.md
+++ readme.md
@@ -12,3 +12,11 @@ O intuito do projeto é aplicar os conhecimentos sobre API REST.
 >>> Funcionalidades
 - Criar um novo post
 - Exclui o post
+
+
+>>>>>>Aprendizado
+
+> dotenv
+Pacote responsável por configurar "VARIÁVEIS DE AMBIENTE", em "DESENVOLVIMENTO", que é configurado no host
+
+Existem as "VARIÁVEIS DE AMBIENTE" em "PRODUÇÃO", que é configurado no serviço de hospedagem
